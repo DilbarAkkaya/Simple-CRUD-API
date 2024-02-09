8a5ae6180148a356631abffbc4242ffcd9c595a0
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-02-10 00:11:20 +0300

    feat: separate func senmessage

--- app.ts
+++ app.ts
@@ -1,4 +1,7 @@
 import { users } from './src/users';
-import { createUser } from './src/api/functions';
+//import { createUser } from './src/api/functions';
+import { createServer } from './src/api/server';
+
 
 console.log(users);
+createServer();
--- src/api/server.ts
+++ src/api/server.ts
@@ -1,15 +1,16 @@
 import http from 'http';
-import { STATUS_CODE, CONTENT_TYPES, HTTP_METHOD, ENDPOINT, PORT } from 'types';
-import { getUsers } from './functions';
+import { STATUS_CODE, HTTP_METHOD, ENDPOINT, PORT } from '../types';
+import { getUsers, sendMessage } from './functions';
 
-const server = http.createServer((req, res) => {
-  if (req.method === HTTP_METHOD.GET && req.url === ENDPOINT) {
-    getUsers(req, res);
-  } else {
-    res.writeHead(STATUS_CODE.NOT_FOUND, { 'Content-Type': CONTENT_TYPES.TextPlain });
-    res.end('Not Found');
-  }
-});
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-})
+export const createServer = () => {
+  const server = http.createServer((req, res) => {
+    if (req.method === HTTP_METHOD.GET && req.url === ENDPOINT) {
+      getUsers(res);
+    } else {
+      sendMessage(res, STATUS_CODE.NOT_FOUND, 'The requested resource was not found');
+    }
+  });
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
