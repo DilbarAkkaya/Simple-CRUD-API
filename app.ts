7b24fc2c09ae509965d2075bae774cf930bb9358
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-02-09 22:47:22 +0300

    feat: create func createuser

--- app.ts
+++ app.ts
@@ -1,2 +1,4 @@
 import { users } from './src/users';
+import { createUser } from './src/api/get';
+
 console.log(users);
--- src/api/get.ts
+++ src/api/get.ts
@@ -1,9 +1,20 @@
 import { users } from '../users';
 import { IncomingMessage, ServerResponse} from 'http';
-import { STATUS_CODE, CONTENT_TYPES } from 'types';
+import { STATUS_CODE, CONTENT_TYPES, User } from 'types';
 
 
  export const getUsers = (req: IncomingMessage, res: ServerResponse) => {
   res.writeHead(STATUS_CODE.OK, { 'Content-Type': CONTENT_TYPES.ApplicationJSON });
   res.end(JSON.stringify(users));
+}
+
+export const createUser = (userReq: User) => {
+  const newUser: User = {
+    id: userReq.id,
+    username: userReq.username,
+    age: userReq.age,
+    hobbies: userReq.hobbies,
+  }
+  users.push(newUser);
+  return newUser;
 }
